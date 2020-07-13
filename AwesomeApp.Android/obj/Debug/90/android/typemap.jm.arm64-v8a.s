97ff5405d356b4999a869ee10d0268f66935ef9e
unknown <abdulrash1144@gmail.com>
2020-07-13 19:59:04 +0100

    Adding Files

---
+++ AwesomeApp.Android/obj/Debug/90/android/typemap.jm.arm64-v8a.s
@@ -0,0 +1,26 @@
+	/* Data Hash: 4B5193087A0CF133 */
+	.arch	armv8-a
+	.file	"typemap.jm.inc"
+
+	/* Mapping header */
+	.section	.data.jm_typemap,"aw",@progbits
+	.type	jm_typemap_header, @object
+	.p2align	2
+	.global	jm_typemap_header
+jm_typemap_header:
+	/* version */
+	.word	1
+	/* entry-count */
+	.word	854
+	/* entry-length */
+	.word	232
+	/* value-offset */
+	.word	102
+	.size	jm_typemap_header, 16
+
+	/* Mapping data */
+	.type	jm_typemap, @object
+	.global	jm_typemap
+jm_typemap:
+	.size	jm_typemap, 198129
+	.include	"typemap.jm.inc"
